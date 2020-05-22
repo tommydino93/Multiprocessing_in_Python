9e7c4b3fa130d361acc8de41e155b83afcfcf640
Tommaso Di Noto <tommydino@hotmail.it>
2020-05-22 18:18:26 +0200

    Update sleep_function_with_return.py

--- sleep_function_with_return.py
+++ sleep_function_with_return.py
@@ -1,8 +1,6 @@
 import multiprocessing as mp
 import time
 
-external_list = []  # type: list
-
 
 def wait(seconds: int):
     """This function takes as input an int number N, waits for N seconds, prints N,
@@ -23,7 +21,7 @@ start_mp = time.time()
 num_workers = mp.cpu_count()  # save number of available CPUs (threads)
 pool = mp.Pool(processes=num_workers)  # create pool object and set as many processes as there are CPUs
 results = [pool.apply_async(wait, args=(i,)) for i in range(1, 5)]
-pool.close()  # prevent any more tasks from being submitted to the pool. Once all the tasks have been completed the worker processes will exit.
+pool.close()  # prevent any more tasks from being submitted to the pool
 pool.join()  # wait for everything on the queue to be processed
 end_mp = time.time()
 print("\nOutputs:")
